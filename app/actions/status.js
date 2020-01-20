82f7e08b4a7ec1dd5544a8067f7e02aee3188fb0
Carlos Bortoni <carlosbortoni@gmail.com>
2020-01-19 23:26:58 -0200

    adicionando redux-thunk

--- app/actions/status.js
+++ app/actions/status.js
@@ -1,6 +1,17 @@
 import { actionTypes } from '../constants/actionTypes.js';
 
-export const changeStatus = userName => ({
-  type: actionTypes.CHANGE_STATUS,
-  payload: `${userName} is typing`
-});
+export const changeStatus = text => 
+    dispatch =>
+        setTimeout(() => 
+            dispatch({
+                type: actionTypes.CHANGE_STATUS,
+                payload: text
+            })
+        , 1000);
+
+// import { actionTypes } from '../constants/actionTypes.js';
+
+// export const changeStatus = userName => ({
+//   type: actionTypes.CHANGE_STATUS,
+//   payload: `${userName} is typing`
+// });
