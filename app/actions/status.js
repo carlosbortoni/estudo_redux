2f5684f6afab10621f52f070714d0740355b4903
Carlos Bortoni <carlosbortoni@gmail.com>
2020-01-19 23:15:05 -0200

    adicionando store, actions, types e reducer

---
+++ app/actions/status.js
@@ -0,0 +1,6 @@
+import { actionTypes } from '../constants/actionTypes.js';
+
+export const changeStatus = userName => ({
+  type: actionTypes.CHANGE_STATUS,
+  payload: `${userName} is typing`
+});
--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
-const statusParagraph = document.querySelector('.status');
+import { store } from './store';
+import { changeStatus } from './actions/status';
 
+const statusParagraph = document.querySelector('.status');
 document
 .querySelector('.userName')
 .oninput = e => {
     const userName = e.target.value;
-    statusParagraph.textContent = userName 
-        ? `${e.target.value} is typing` 
-        : '';
+    store.dispatch(changeStatus(userName));
 };
+
+store.subscribe(() => {
+    // obtemos o estado atual, depois da modificação
+    const state = store.getState();
+    // atualizamos o elemento com o novo valor
+    statusParagraph.textContent = state.status;
+});
